83c9b83577e86039f1fcc946cdbcac0cddab456b
helloworldforeverlove <134216852+helloworldforeverlove@users.noreply.github.com>
2024-04-30 19:53:37 +0000

    eeeeeeeeeeeeeee

--- src/components/Transactions/components/CategorySelectionModal.jsx
+++ src/components/Transactions/components/CategorySelectionModal.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import {
   Box, Flex, IconButton, Modal, ModalOverlay, ModalContent, ModalHeader,
-  ModalBody, SimpleGrid, Tag, VStack, Heading, FormControl, FormLabel, Input, Container
+  ModalBody, SimpleGrid, Tag, VStack, Heading, FormControl, FormLabel, Input, Container, useColorModeValue,
 } from '@chakra-ui/react';
 import { FaTimes } from 'react-icons/fa';
+import { FcFullTrash, FcBullish, FcDebt, FcFactory, FcAutomotive, FcAlarmClock, FcDonate } from 'react-icons/fc';
 
 function CategorySelectionModal({ categories, isOpen, onClose, onSelectCategory, activeIndex }) {
+  const icons = {
+    Revenues: <FcBullish />,
+    Remunerations: <FcDebt />,
+    Functionnement: <FcFactory />,
+    Deplacements: <FcAutomotive />,
+    FraisFixes: <FcAlarmClock />,
+    CotisationsEtTaxes: <FcDonate />
+  };
+
+  const iconColor = useColorModeValue('gray.200', 'gray.300');
+  const bgColor = useColorModeValue('gray.50', 'gray.700');
+  const borderColor = useColorModeValue('gray.200', 'gray.600');
+  const hoverBg = useColorModeValue("green.100", "green.700");
+  const activeBg = useColorModeValue("blue.300", "blue.800");
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="full" overflow="auto">
       <ModalOverlay />
@@ -23,21 +38,26 @@ function CategorySelectionModal({ categories, isOpen, onClose, onSelectCategory,
           />
         </ModalHeader>
         <ModalBody>
-          <Container maxW="container.xxl">
-            <SimpleGrid columns={6} spacing={5}>
+            <Container maxW="container.xxl">
+              <SimpleGrid columns={6} spacing={5}>
               {Object.keys(categories).map((categoryKey) => (
                 <Box p={5} borderWidth="1px" borderRadius="lg" key={categoryKey}>
-                  <Flex align="center" fontSize="xl">
-                    <Heading as="h3" ml={3} fontSize="xl">{categoryKey}</Heading>
+                <Flex align="center" fontSize="xl">
+                  {icons[categoryKey]}
+                  <Heading as="h3" ml={3} fontSize="xl">{categoryKey}</Heading>
                   </Flex>
                   <VStack align="start" spacing={3}>
                     {categories[categoryKey].map((item, index) => (
-                      <Tag
-                        key={index}
-                        size="md"
-                        variant="solid"
-                        _hover={{ bg: "blue.500", color: "white" }}
-                        onClick={() => onSelectCategory(item, activeIndex)}
+                      <Tag size="md" variant="solid" key={index} _hover={{
+                        background: hoverBg,
+                        transform: 'scale(1.1)',
+                        transition: 'background-color 0.2s, transform 0.2s'
+                      }} _active={{
+                        background: activeBg,
+                        transform: 'scale(0.9)',
+                        transition: 'background-color 0.1s, transform 0.1s'
+                      }} 
+                      onClick={() => onSelectCategory(item, activeIndex)}
                       >
                         {item}
                       </Tag>

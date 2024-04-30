d8c17dc942bb1ef9711f3ab5588c9f209e2f5aec
helloworldforeverlove <134216852+helloworldforeverlove@users.noreply.github.com>
2024-04-30 17:53:00 +0000

    dffffr

--- src/components/Transactions/components/TransactionItem.jsx
+++ src/components/Transactions/components/TransactionItem.jsx
@@ -46,56 +46,56 @@ function TransactionItem({ transaction }) {
   const { isOpen: isDetailOpen, onOpen: onDetailOpen, onClose: onDetailClose, onToggle: onDetailToggle } = useDisclosure();
   const { isOpen: isAnnotationModalOpen, onOpen: onAnnotationModalOpen, onClose: onAnnotationModalClose } = useDisclosure();
   const { isOpen: isVentilationModalOpen, onOpen: onVentilationModalOpen, onClose: onVentilationModalClose } = useDisclosure();
-  
+
 
   const [ventilations, setVentilations] = useState(transaction.ventilations || []);
   const [tempVentilations, setTempVentilations] = useState(ventilations);
   // Autres hooks et fonctions inchangés...
   const newVentilation = () => {
     return { id: ventilations.length + 1, category: '', amount: '' };
-};
-const addVentilation = () => {
-  setTempVentilations([...tempVentilations, newVentilation()]);
-};
-const deleteVentilation = (index) => {
-  setTempVentilations(prevVentilations => prevVentilations.filter((_, i) => i !== index));
-};
-
-const submitVentilations = async () => {
-  const payload = {
-    ventilations: tempVentilations.map(v => ({
-      id: v.id,
-      amount: v.amount,
-      category: v.category
-    }))
+  };
+  const addVentilation = () => {
+    setTempVentilations([...tempVentilations, newVentilation()]);
+  };
+  const deleteVentilation = (index) => {
+    setTempVentilations(prevVentilations => prevVentilations.filter((_, i) => i !== index));
   };
 
-  const { data, error } = await supabase
-    .from('transactions')
-    .update(payload)
-    .eq('id', transaction.id);
+  const submitVentilations = async () => {
+    const payload = {
+      ventilations: tempVentilations.map(v => ({
+        id: v.id,
+        amount: v.amount,
+        category: v.category
+      }))
+    };
+
+    const { data, error } = await supabase
+      .from('transactions')
+      .update(payload)
+      .eq('id', transaction.id);
 
     if (error) {
-        toast({
-            title: "Error updating ventilations",
-            description: error.message,
-            status: "error",
-            duration: 5000,
-            isClosable: true,
-        });
-        console.error("Error updating ventilations:", error);
+      toast({
+        title: "Error updating ventilations",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      console.error("Error updating ventilations:", error);
     } else {
-        setVentilations([...tempVentilations]);
-        toast({
-            title: "Ventilations Updated",
-            description: "Ventilations have been successfully updated.",
-            status: "success",
-            duration: 5000,
-            isClosable: true,
-        });
-        console.log("Ventilations updated successfully:", data);
+      setVentilations([...tempVentilations]);
+      toast({
+        title: "Ventilations Updated",
+        description: "Ventilations have been successfully updated.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      console.log("Ventilations updated successfully:", data);
     }
-};
+  };
 
 
   const bgColor = useColorModeValue('gray.50', 'gray.700');
@@ -104,7 +104,7 @@ const submitVentilations = async () => {
   const hoverBgColor = useColorModeValue('gray.100', 'gray.600');
   const toast = useToast();
   const [activeVentilationIndex, setActiveVentilationIndex] = useState(null);
-  const [selectedItem, setSelectedItem] = useState('');  
+  const [selectedItem, setSelectedItem] = useState('');
 
   const categories = {
     Revenues: ['Apport personnel', 'Recette', 'Recette secondaire', 'Redevance de collaboration perçue', 'Autre gain divers', 'Vente d’une immobilisation', 'Emprunt', 'Caution reçue'],
@@ -134,12 +134,12 @@ const submitVentilations = async () => {
     newVentilations[index] = { ...newVentilations[index], category };
     setTempVentilations(newVentilations);
   };
-  
+
   const handleAmountChange = (index, amount) => {
     const newVentilations = [...tempVentilations];
     newVentilations[index] = { ...newVentilations[index], amount: parseFloat(amount) };
     setTempVentilations(newVentilations);
-  };  
+  };
 
 
   const [annotation, setAnnotation] = useState('');
@@ -275,69 +275,65 @@ const submitVentilations = async () => {
             <Box p={4} bg={bgColor} borderRadius="lg" borderWidth="1px" borderColor={borderColor}>
               <Text fontSize="lg" fontWeight="semibold" mb={4}>Ventilation(s)</Text>
               {tempVentilations.map((ventilation, index) => (
-  <Box key={ventilation.id} mb={4} p={4} bg="white" borderRadius="lg" boxShadow="sm">
-    <Flex justify="space-between" align="center">
-      <Text fontWeight="medium">Ventilation {index + 1}</Text>
-      <IconButton
-        aria-label="Delete ventilation"
-        icon={<FaTimes />}
-        size="sm"
-        variant="ghost"
-        colorScheme="red"
-        onClick={() => deleteVentilation(index)}
-      />
-    </Flex>
-    <Stack spacing={4} mt={4}>
-      <FormControl>
-        <FormLabel>Catégorie</FormLabel>
-        <Select
-          placeholder="Sélectionnez une catégorie..."
-          value={ventilation.category}
-          onChange={(e) => handleCategorySelect(index, e.target.value)}
-        >
-          {Object.keys(categories).map(categoryKey =>
-            categories[categoryKey].map(item => (
-              <option value={item} key={item}>{item}</option>
-            ))
-          )}
-        </Select>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Montant</FormLabel>
-        <InputGroup>
-          <Input type="number" value={ventilation.amount || ''} onChange={(e) => handleAmountChange(index, e.target.value)} />
-          <InputRightElement pointerEvents="none" children={<MdEuro color="gray.500" />} />
-        </InputGroup>
-      </FormControl>
-    </Stack>
-  </Box>
-))}
+                <Box key={ventilation.id} mb={4} p={4} bg="white" borderRadius="lg" boxShadow="sm">
+                  <Flex justify="space-between" align="center">
+                    <Text fontWeight="medium">Ventilation {index + 1}</Text>
+                    <IconButton
+                      aria-label="Delete ventilation"
+                      icon={<FaTimes />}
+                      size="sm"
+                      variant="ghost"
+                      colorScheme="red"
+                      onClick={() => deleteVentilation(index)}
+                    />
+                  </Flex>
+                  <Stack spacing={4} mt={4}>
+                    <FormControl>
+                      <FormLabel>Catégorie</FormLabel>
+                      <Select
+                        placeholder="Sélectionnez une catégorie..."
+                        value={ventilation.category}
+                        onChange={(e) => handleCategorySelect(index, e.target.value)}
+                      >
+                        {Object.keys(categories).map(categoryKey =>
+                          categories[categoryKey].map(item => (
+                            <option value={item} key={item}>{item}</option>
+                          ))
+                        )}
+                      </Select>
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel>Montant</FormLabel>
+                      <InputGroup>
+                        <Input type="number" value={ventilation.amount || ''} onChange={(e) => handleAmountChange(index, e.target.value)} />
+                        <InputRightElement pointerEvents="none" children={<MdEuro color="gray.500" />} />
+                      </InputGroup>
+                    </FormControl>
+                  </Stack>
+                </Box>
+              ))}
               <Button leftIcon={<FaPlus />} colorScheme="blue" variant="outline" onClick={addVentilation} mt={2}>
                 Ajouter une ventilation
               </Button>
             </Box>
           </ModalBody>
           <ModalFooter>
-  <Tooltip label="Apply changes to all ventilations" placement="top">
-  <Button
-    leftIcon={<Icon as={FcSupport} />}
-    colorScheme="blue"
-    onClick={submitVentilations}
-    mr={2}
-    variant="ghost"
-    aria-label="Apply changes"
-  >
-    Enregistrer
-  </Button>
-  </Tooltip>
-  <Button onClick={onVentilationModalClose}>Close</Button>
-</ModalFooter>
-
+            <Tooltip label="Apply changes to all ventilations" placement="top">
+              <Button
+                leftIcon={<Icon as={FcSupport} />}
+                colorScheme="blue"
+                onClick={submitVentilations}
+                mr={2}
+                variant="ghost"
+                aria-label="Apply changes"
+              >
+                Enregistrer
+              </Button>
+            </Tooltip>
+            <Button onClick={onVentilationModalClose}>Close</Button>
+          </ModalFooter>
         </ModalContent>
       </Modal>
-
-
-
     </>
   );
 }
